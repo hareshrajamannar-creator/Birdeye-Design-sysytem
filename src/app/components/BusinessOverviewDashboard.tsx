ddcd3344d931b3745697b3930b766519f3e7be13
Haresh Rajamannar <haresh.rajamannar@birdeye.com>
2026-04-24 16:25:09 +0530

    feat: convert all built screens to consistent ModuleEmptyState

--- src/app/components/BusinessOverviewDashboard.tsx
+++ src/app/components/BusinessOverviewDashboard.tsx
@@ -1,2 +1,10 @@
-export * from "./BusinessOverviewDashboard.v1";
-export { default } from "./BusinessOverviewDashboard.v1";
+// Reference implementation preserved in BusinessOverviewDashboard.v1.tsx
+import { useActiveL2Item } from "@/app/context/L2NavBridgeContext";
+import { ModuleEmptyState } from "@/app/components/layout/ModuleEmptyState";
+
+export function BusinessOverviewDashboard() {
+  const activeItem = useActiveL2Item();
+  return <ModuleEmptyState moduleName="Home" activeL2Key={activeItem} />;
+}
+
+export default BusinessOverviewDashboard;
